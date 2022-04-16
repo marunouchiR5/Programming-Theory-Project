36bfec42f3220266ad5ba68b65b34798d9beb38c
marunouchiR5 <53039597+marunouchiR5@users.noreply.github.com>
2022-04-16 17:27:39 +0800

    main gameplay
    - added basic game objects and gameplay
    - added popping text indicating colors

---
+++ Assets/Scripts/Capsule.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Capsule : Shape
+{
+    [SerializeField] private float delay = 1.0f;
+    
+    public string Color { get; private set; }
+    public TMP_Text ColorText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Color = "Blue";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            DisplayText();
+        }
+    }
+    
+    protected override void DisplayText()
+    {
+        ColorText.enabled = true;
+        ColorText.text = Color;
+        StartCoroutine(EnableText());
+    }
+
+    IEnumerator EnableText()
+    {
+        yield return new WaitForSeconds(delay);
+        ColorText.enabled = false;
+    }
+}
---
+++ Assets/Scripts/Shape.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+    protected virtual void DisplayText()
+    {
+        
+    }
+}
